8a83e6d202dc423e4e7a3ca8e8368f32e382ce6b
Gladsonms <gladsonsaji4@gmail.com>
2021-08-20 10:54:08 +0530

    improved validation with on blur

--- script.js
+++ script.js
@@ -1,88 +1,105 @@
-function validation(){
     
-    var name=document.getElementById("name").value;
-    var phone=document.getElementById("phone").value;
-    var email=document.getElementById("email").value;
-    var message=document.getElementById("message").value;
+   
     var text;
-
-
-
-    if(name.length==0){
-        text="Please Enter your Name";
-        nameError.innerHTML = text;
-        return false;
-    } else{ text=" "
-    nameError.innerHTML=text;
-}
-
-    if(name.length<3||name.length>20||name.length==" "|| name.includes('  ')){
-        text="Please Enter Valid Name";
-        nameError.innerHTML = text;
-        return false;
-    }else{text=" "
-    nameError.innerHTML=text;
-}
-   if(isNaN(phone) || phone.length==0 || phone.includes('e')){
-        text="Please Enter your Number";
-        phoneError.innerHTML = text;
-        return false;
-}    
-
-
-    if(isNaN(phone) || phone.length!=10){
-        text="Please Enter Valid Number";
-        phoneError.innerHTML = text;
-        return false;
-    }else{
-        text=" "
-        phoneError.innerHTML=text;
+    var nameRegex=/^[a-zA-Z ]+$/;
+    var emailRegex=/"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"/
+    // Check name
+    var nameField=document.getElementById("name")
+    nameField.addEventListener("blur",()=>
+    {
+        var name = nameField.value
+        if(name.length==0){
+            text="Please Enter your Name";
+            nameError.innerHTML = text;
+            
+        } else if(name.length<3||name.length>20||name.includes('  ')|| !name.match(nameRegex)){
+            text="Please Enter Valid Name";
+            nameError.innerHTML = text;
+            
+        }else{text=" "
+        nameError.innerHTML=text;
     }
+    })
+    
+    //check phone
+    var phoneField=document.getElementById("phone");
+    phoneField.addEventListener("blur",()=>{
+        var phone=phoneField.value
+        if(isNaN(phone) || phone.length==0 || phone.includes('e')){
+            text="Please Enter your Number";
+            phoneError.innerHTML = text;
+            
+    }    
+    
+    
+        if(isNaN(phone) || phone.length!=10){
+            text="Please Enter Valid Number";
+            phoneError.innerHTML = text;
+            
+        }else{
+            text=" "
+            phoneError.innerHTML=text;
+        }
+    
 
+    })
+  
     
 
 
+  //check mail
+    var emailField=document.getElementById("email");
 
-
+    emailField.addEventListener("blur",()=>{
+        var email = emailField.value;
+        if(email.length==0){
+            text="Please Enter Your E-mail";
+            mailError.innerHTML = text;
+            
+        }
     
-    if(email.length==0){
-        text="Please Enter Your E-mail";
-        mailError.innerHTML = text;
-        return false;
-    }
-
-    if(email.indexOf("@")== -1 || email.length<6){
-        text="Please Enter Valid E-mail";
-        mailError.innerHTML = text;
-        return false;
-    }else{
-        text=" "
-        mailError.innerHTML=text;
-    } 
-
+        if(email.indexOf("@")== -1 || email.length<6 || !email.match(emailRegex)){
+            text="Please Enter Valid E-mail";
+            mailError.innerHTML = text;
+           
+        }else{
+            text=" "
+            mailError.innerHTML=text;
+        } 
+    
+    })
+    
+    
+    
+      
+         //check messsage
+        var messageFiled=document.getElementById("message");
+        var message = messageFiled.value;
+        message.addEventListener("blur",()=>{
     if(message.length==0){
         text="Please Enter Your Message"
         messageError.innerHTML = text;
-        return false;
+        
     } 
-
-
-    if(message.length<=40){
-        text="Please Enter More Than 40 Characters"
-        messageError.innerHTML = text;
-        return false;
-    }else{
-        text=" "
-        messageError.innerHTML=text;
-    }
-
+            if(message.length<=40){
+                text="Please Enter More Than 40 Characters"
+                messageError.innerHTML = text;
+                //return false;
+            }else{
+                text=" "
+                messageError.innerHTML=text;
+            }
+        
+        })
+       
+    
+        
     alert("Submitted Succesfully!")
     data = {
         name, phone, email, message }
     mail(data)
-    return false;
 
-}
+
 
 function mail(data) {
         $.ajax({
