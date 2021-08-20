5381e95f5b7e30470d3081f29af5923efb1c7024
Gladsonms <gladsonsaji4@gmail.com>
2021-08-20 08:41:49 +0530

    fixed bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function validation(){
-    console.log('validation');
+    
     var name=document.getElementById("name").value;
     var phone=document.getElementById("phone").value;
     var email=document.getElementById("email").value;
@@ -10,33 +10,33 @@ function validation(){
 
     if(name.length==0){
         text="Please Enter your Name";
-        error_name.innerHTML = text;
+        nameError.innerHTML = text;
         return false;
     } else{ text=" "
-error_name.innerHTML=text;
+    nameError.innerHTML=text;
 }
 
     if(name.length<3||name.length>20||name.length==" "|| name.includes('  ')){
         text="Please Enter Valid Name";
-        error_name.innerHTML = text;
+        nameError.innerHTML = text;
         return false;
     }else{text=" "
-error_name.innerHTML=text;
+    nameError.innerHTML=text;
 }
    if(isNaN(phone) || phone.length==0 || phone.includes('e')){
         text="Please Enter your Number";
-        error_phone.innerHTML = text;
+        phoneError.innerHTML = text;
         return false;
 }    
 
 
     if(isNaN(phone) || phone.length!=10){
         text="Please Enter Valid Number";
-        error_phone.innerHTML = text;
+        phoneError.innerHTML = text;
         return false;
     }else{
         text=" "
-        error_phone.innerHTML=text;
+        phoneError.innerHTML=text;
     }
 
     
@@ -47,33 +47,33 @@ error_name.innerHTML=text;
     
     if(email.length==0){
         text="Please Enter Your E-mail";
-        error_mail.innerHTML = text;
+        mailError.innerHTML = text;
         return false;
     }
 
     if(email.indexOf("@")== -1 || email.length<6){
         text="Please Enter Valid E-mail";
-        error_mail.innerHTML = text;
+        mailError.innerHTML = text;
         return false;
     }else{
         text=" "
-        error_mail.innerHTML=text;
+        mailError.innerHTML=text;
     } 
 
     if(message.length==0){
         text="Please Enter Your Message"
-        error_message.innerHTML = text;
+        messageError.innerHTML = text;
         return false;
     } 
 
 
     if(message.length<=40){
         text="Please Enter More Than 40 Characters"
-        error_message.innerHTML = text;
+        messageError.innerHTML = text;
         return false;
     }else{
         text=" "
-        error_message.innerHTML=text;
+        messageError.innerHTML=text;
     }
 
     alert("Submitted Succesfully!")
